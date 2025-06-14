90b3459797aa16242bd65c61679a7e38ab1f7bf3
GirishCodes <girishyadav.cs@gmail.com>
2025-06-14 15:56:06 +0530

    Create input-output.cpp

---
+++ Lec 1 Things to know In C++ or Any other language - Copy/input-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//Code Starts Here
+class Solution {
+public:
+    void printNumber() {
+        int num;
+        cin >> num;
+        cout << num;
+    }
+};
+// Code Ends Here
+int main() {
+    Solution sol;
+    sol.printNumber();
+    return 0;
+}
